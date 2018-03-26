61d240dd70aebf04681b8d5cb803ea4198fd41d2
ubuntu <ubuntu@localhost.localdomain>
2018-03-26 21:12:59 +0800

    add main

---
+++ engine/CMakeFiles/osv.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/osv.dir/main.cpp.o"
+  "osv.pdb"
+  "osv"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/osv.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
